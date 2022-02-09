d99baaa61c6de4c44d75044004643bb5e3cdccbc
ludfyrahman <ludfyr@gmail.com>
2022-02-09 22:49:07 +0700

    event

--- application/controllers/api/dosen/Event.php
+++ application/controllers/api/dosen/Event.php
@@ -60,8 +60,8 @@ class Event extends REST_Controller
 	}
     public function detail_data_get($id)
 	{
-		$data = $this->db->query("SELECT * FROM events e 
-        JOIN tb_user tu ON e.id_user=tu.id_user where e.id='$id'")->row_array();
+		$data = $this->db->query("SELECT * FROM tb_event e 
+        JOIN tb_user tu ON e.id_user=tu.id_user where e.id='$id' and tu.id_role=3")->row_array();
         $response = [];
         if($data){
             $response = [
--- application/controllers/api/mahasiswa/Event.php
+++ application/controllers/api/mahasiswa/Event.php
@@ -39,7 +39,7 @@ class Event extends REST_Controller
 	}
     public function pengajuan_get($id)
 	{
-		$data = $this->db->query("SELECT * FROM tb_event e JOIN tb_user tu ON e.id_dosen=tu.id_user where e.id_user=$id")->result_array();
+		$data = $this->db->query("SELECT e.*, tu.name FROM tb_event e JOIN tb_user tu ON e.id_dosen=tu.id_user where e.id_user=$id")->result_array();
         $response = [];
         if(count($data) > 0){
             $response = [
@@ -58,7 +58,7 @@ class Event extends REST_Controller
 	}
     public function detail_pengajuan_get($id)
 	{
-		$data = $this->db->query("SELECT * FROM tb_event e JOIN tb_user tu ON e.id_user=tu.id_user where e.id=$id AND tu.id_role=3")->row_array();
+		$data = $this->db->query("SELECT * FROM tb_event e JOIN tb_user tu ON e.id_dosen=tu.id_user where e.id=$id AND tu.id_role=2")->row_array();
         $response = [];
         if($data){
             $response = [
@@ -98,4 +98,42 @@ class Event extends REST_Controller
         }
         $this->response($response, \Restserver\Libraries\REST_Controller::HTTP_OK);
     }
+    public function delete_get($id)
+	{
+            $data = $this->db->delete("tb_event", ['id' => $id]);
+            $response = [];
+            if($data){
+                $response = [
+                    'status' => true,
+                    'message' => "Data Berhasil dihapus"
+                ];
+            }else{
+                $response = [
+                    'status' => false,
+                    'data' => $data,
+                    'message' => "Data Gagal dihapus"
+                ];
+            }
+            $this->response($response, \Restserver\Libraries\REST_Controller::HTTP_OK);
+        
+	}
+    public function batal_get($id)
+	{
+            $data = $this->db->update("tb_event", ['status' => 'batal'], ['id' => $id]);
+            $response = [];
+            if($data){
+                $response = [
+                    'status' => true,
+                    'message' => "Data Berhasil dibatalkan"
+                ];
+            }else{
+                $response = [
+                    'status' => false,
+                    'data' => $data,
+                    'message' => "Data Gagal dibatalkan"
+                ];
+            }
+            $this->response($response, \Restserver\Libraries\REST_Controller::HTTP_OK);
+        
+	}
 }
